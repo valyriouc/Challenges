d762977802d26f7af993a9a573afbb396f7b067a
valyriouc <valyrious.informatics@gmail.com>
2023-01-14 22:09:47 +0100

    Reversed sequence

---
+++ reversed-sequence/reversed-sequence.py
@@ -0,0 +1,15 @@
+
+def reverse_seq(n):
+    lst = []
+    for i in range(n, 0, -1):
+        lst.append(i)
+    return lst
+
+def reverse_seq(n):
+    return [i for i in range(n, 0, -1)]
+
+reverse_seq = lambda n: [i for i in range(n, 0, -1)]
+
+def reverse_seq(n):
+    return range(n, 0, -1)
+

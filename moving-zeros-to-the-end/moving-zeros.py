6a0b18f5c29525d3140b2faab797985c38a1aad9
valyriouc <valyrious.informatics@gmail.com>
2023-02-11 13:57:51 +0100

    Another solution for moving zeros

--- moving-zeros-to-the-end/moving-zeros.py
+++ moving-zeros-to-the-end/moving-zeros.py
@@ -11,5 +11,7 @@ def move_zeros(lst: list):
     lst.extend(nulls)
     return lst
 
-if __name__ == '__main__':
-    print (move_zeros([]))
+def move_zeros(lst: list):
+    l = [i for i in lst if i != 0]
+    l.extend([0 for i in range(0, len(lst) - len(l))])
+    return l
